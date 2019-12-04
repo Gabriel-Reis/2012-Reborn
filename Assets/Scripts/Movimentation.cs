c2d777c2c60af7dd7c31e87c23a2efb64a74fbc9
Gabriel Augusto <g.requenareis@gmail.com>
2019-12-04 03:46:18 -0300

    Camera, movimentation
    Camera adjusted
    Movimentation fixed

---
+++ Assets/Scripts/Movimentation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Movimentation : MonoBehaviour {
+    Animator anim;
+
+    void Start () {
+        anim = GetComponent<Animator> ();
+    }
+
+    void Update () {
+        
+        var estaMovendo = Input.GetAxis("Vertical");
+        if (estaMovendo != 0) {
+            anim.SetBool("estaAndando", true);
+        } else {
+            anim.SetBool("estaAndando", false);
+        }
+    }
+}

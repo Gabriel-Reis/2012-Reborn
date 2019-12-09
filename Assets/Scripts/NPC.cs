c3a299f6397a02e890395838fc370bb4e44aa0c1
Gabriel Augusto <g.requenareis@gmail.com>
2019-12-09 01:42:49 -0300

    Score e objectives

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 [System.Serializable]
 public class NPC : MonoBehaviour {
@@ -15,6 +16,12 @@ public class NPC : MonoBehaviour {
 
     [TextArea(5, 10)]
     public string[] sentences;
+    // SCORE
+    public Text scoreText;
+    public int objetiveNumber;
+    public Text objectiveText; //Textos dos objetivos
+    private bool[] booleans = new bool[7] {false,false,false,false,false,false,false};
+
 
     void Start () {
         dialogueSystem = FindObjectOfType<DialogueSystem>();
@@ -36,7 +43,39 @@ public class NPC : MonoBehaviour {
             dialogueSystem.Names = Name;
             dialogueSystem.dialogueLines = sentences;
             FindObjectOfType<DialogueSystem>().NPCName();
+            //score
+            if(!booleans[objetiveNumber+1]){
+                int score;
+                if(objetiveNumber >= 5){
+                    score = Convert.ToInt32(scoreText.text);
+                    score += 20;
+                }                
+                else{
+                    score = Convert.ToInt32(scoreText.text);
+                    score += 10;
+                }
+                scoreText.text = score.ToString();
+                UpdateObjectivesText(objetiveNumber);
+            }
+            booleans[objetiveNumber+1] = true;
         }
+    }
+
+    public void UpdateObjectivesText(int objective){
+        if(objective == 1)
+            objectiveText.text = " ̶D̶e̶s̶c̶o̶b̶r̶i̶r̶ ̶o̶ ̶p̶a̶p̶i̶r̶o̶";
+        else if(objective == 2)
+            objectiveText.text = "\n ̶D̶e̶s̶c̶o̶b̶r̶i̶r̶ ̶h̶i̶e̶r̶ó̶g̶l̶i̶f̶o̶s̶";
+        else if(objective == 3)
+            objectiveText.text = "\n\n ̶D̶e̶s̶c̶o̶b̶r̶i̶r̶ ̶t̶ú̶m̶u̶l̶o̶ ̶d̶a̶ ̶r̶a̶i̶n̶h̶a̶";
+        else if(objective == 4)
+            objectiveText.text = "\n\n\n ̶D̶e̶s̶c̶o̶b̶r̶i̶r̶ ̶t̶ú̶m̶u̶l̶o̶ ̶d̶o̶ ̶r̶e̶i̶";
+        else if(objective == 5)
+            objectiveText.text = "\n\n\n\n ̶F̶a̶l̶a̶r̶ ̶c̶o̶m̶ ̶A̶l̶p̶h̶y̶s̶";
+        else if(objective == 6)
+            objectiveText.text = "\n\n\n\n\n ̶F̶a̶l̶a̶r̶ ̶c̶o̶m̶ ̶W̶h̶e̶a̶t̶l̶e̶y̶";
+        else if(objective == 7)
+            objectiveText.text = "\n\n\n\n\n\n ̶F̶a̶l̶a̶r̶ ̶c̶o̶m̶ ̶A̶k̶i̶r̶a̶";
     }
 
     public void OnTriggerExit()
@@ -44,4 +83,5 @@ public class NPC : MonoBehaviour {
         FindObjectOfType<DialogueSystem>().OutOfRange();
         this.gameObject.GetComponent<NPC>().enabled = false;
     }
+
 }
